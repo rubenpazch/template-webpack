319fafe497d7ac4ae764dcc616d455d6e74aed6f
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-06-17 20:51:21 -0500

    add initial file for webpack projects

---
+++ src/myName.js
@@ -0,0 +1,3 @@
+const myName = (name) => 'Hi! My name is not ' + name;
+
+export default myName
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module:{
+    rules:[
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.css$/,
+        use:[
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
